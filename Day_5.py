11ec9f98e7e06910887c7c9d18ab276dd1eeb4b6
gracenlov <45708884+gracenlov@users.noreply.github.com>
2024-01-15 09:00:09 -0800

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -6,3 +6,13 @@ list=[3,9,212,-4,-90,43,542,1003]
 min_value=min(list)
 max_value=max(list)
 print("The minimum value from the list is {} and the maximum value from the list is {}".format(min_value,max_value))
+
+#get user input
+input_list=input("Enter list of number separated by a space: ")
+user_list=input_list.split()
+for i in range(len(user_list)):
+    user_list[i]=int(user_list[i])
+
+min_value=min(user_list)
+max_value=max(user_list)
+print("The minimum value from the list is {} and the maximum value from the list is {}".format(min_value,max_value))
