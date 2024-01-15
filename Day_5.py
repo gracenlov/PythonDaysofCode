af900d0b543e76326756f84ea600e5bf21f8b679
gracenlov <45708884+gracenlov@users.noreply.github.com>
2024-01-15 08:51:59 -0800

    Create Day_5.py
    find the maximum and minimum values in the list

---
+++ Day_5.py
@@ -0,0 +1,8 @@
+#create a program to find the maximum and minimum values in a list
+
+
+list=[3,9,212,-4,-90,43,542,1003]
+
+min_value=min(list)
+max_value=max(list)
+print("The minimum value from the list is {} and the maximum value from the list is {}".format(min_value,max_value))
