656fba1ecffde7b7ce05b7ea1ea173ba2035a77d
gracenlov <45708884+gracenlov@users.noreply.github.com>
2024-01-30 19:21:29 -0800

    Update Day_20.py

--- Day_20.py
+++ Day_20.py
@@ -8,7 +8,9 @@ def get_even_numbers_list(num_list):
             even_num_list.append(num)
     return even_num_list
 
-temp=[12,2,255,24,54,55,56,58,57,17,55,77,79,36,37,35]
-print("list of numbers: ", temp)
-result_list=get_even_numbers_list(temp)
+user_input=[int(item) for item in input("Enter a list of numbers separated by comma: ").split(",")]
+
+#temp=[12,2,255,24,54,55,56,58,57,17,55,77,79,36,37,35]
+print("list of numbers: ", user_input)
+result_list=get_even_numbers_list(user_input)
 print("even number list: ", result_list)
