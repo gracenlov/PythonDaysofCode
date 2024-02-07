b712cd32e76c17e082119516c0b0ac2bccb5df90
gracenlov <45708884+gracenlov@users.noreply.github.com>
2024-02-06 23:53:16 -0800

    Create RemoveNthElement.py

---
+++ RemoveNthElement.py
@@ -0,0 +1,15 @@
+#create a program that removes the nth element from a list
+
+
+def removeNthElement(xList,nthEle):
+    if(nthEle<len(userInput1) and nthEle>-1):
+        xList.pop(nthEle)
+    return xList
+
+userInput1=list(x for x in input("enter list 1 using comma separated: ").split(","))
+nthElement=int(input("enter element # to be removed:"))
+print(userInput1)
+
+result_list=removeNthElement(userInput1,nthElement)
+
+print("list: ", result_list)
