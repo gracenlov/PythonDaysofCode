6c440be41df33a4125d8fcc9d88a42edc4a46af7
gracenlov <45708884+gracenlov@users.noreply.github.com>
2024-02-23 20:48:48 -0800

    Update RemoveStringWhitespaces.py

--- RemoveStringWhitespaces.py
+++ RemoveStringWhitespaces.py
@@ -1,7 +1,9 @@
 #program that removes all whitespaces from a given string
 
-string1="te st ing\n and this\t is \v another"
+string1="te st ing\n and this\t is \v another\r again"
 def remove_whitespaces(string1):
-  return string1.replace(" ","").replace("\n","").replace("\v","").replace("\t","")
+  return string1.replace(" ","").replace("\n","").replace("\v","").replace("\t","").replace("\f","").replace("\r","")
 
 print(remove_whitespaces(string1))
+
+
