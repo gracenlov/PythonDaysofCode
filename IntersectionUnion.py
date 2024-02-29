70fe3739ca8dccc0af27d05b9d21bb624a2cf54a
gracenlov <45708884+gracenlov@users.noreply.github.com>
2024-02-28 19:56:18 -0800

    Create IntersectionUnion.py

---
+++ IntersectionUnion.py
@@ -0,0 +1,7 @@
+#create a program that finds the intersection and union of two sets
+
+set1={"apple","banana","grape","cherry","peach","fig","plum"}
+set2={"persimmon","apple","peach","pear","nectarine"}
+
+print("sets intersection: ",set1.intersection(set2))
+print("sets union: ",set1.union(set2))
